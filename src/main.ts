4c47f47708219ddf67359491d9392d6a91959ad9
Facug03 <facundogonzales1550@gmail.com>
2024-05-30 12:29:34 -0300

    remvoe Game class

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import { Game } from './game/Game'
-import { Board } from './game/board/Board'
+import { Board } from './game/Board'
 import './style.css'
 
 const board = new Board()
-const game = new Game(board)
-
-game.startGame()
+board.startGame()
