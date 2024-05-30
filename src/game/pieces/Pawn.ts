aa039ede3bd82bb464e892cee858eb6a2a36e57f
Facug03 <facundogonzales1550@gmail.com>
2024-05-30 12:29:52 -0300

    add movement logic

--- src/game/pieces/Pawn.ts
+++ src/game/pieces/Pawn.ts
@@ -1,5 +1,6 @@
 import { PIECES } from '../../consts/pieces'
-import { Color, Piece, PieceName, PiecePosition } from '../../types'
+import { ChessBoard, Color, Piece, PieceName, PiecePosition } from '../../types'
+import { Empty } from './Empty'
 
 export class Pawn implements Piece {
   public name: PieceName = PIECES.pawn
@@ -16,5 +17,85 @@ export class Pawn implements Piece {
 
   setPosition(position: PiecePosition): void {
     this.position = position
+  }
+
+  movePieceTo(moveTo: PiecePosition, board: ChessBoard): boolean {
+    const [fromX, fromY] = this.position
+    const [toX, toY] = moveTo
+    const squaresToMoveX = fromX - toX
+    const squaresToMoveY = fromY - toY
+    const currentPiece = board[fromX][fromY]
+
+    console.log(
+      { squaresToMoveX, squaresToMoveY },
+      this.checkColision(moveTo, board)
+    )
+
+    if (squaresToMoveX <= 0 || squaresToMoveX > 2) {
+      return false
+    }
+
+    if (squaresToMoveX === 2 && squaresToMoveY !== 0) {
+      return false
+    }
+
+    if (this.checkColision(moveTo, board)) {
+      return false
+    }
+
+    board[toX][toY] = currentPiece
+    board[fromX][fromY] = new Empty('empty', [fromX, fromY], '')
+    this.setPosition(moveTo)
+
+    return true
+  }
+
+  checkColision(moveTo: PiecePosition, board: ChessBoard): boolean {
+    const [toX, toY] = moveTo
+    const { squaresToMoveX, squaresToMoveY } = this.squaresToMove(moveTo)
+    const moveToBoardPosition = board[toX][toY]
+
+    if (squaresToMoveX === 2 && squaresToMoveY === 0) {
+      if (moveToBoardPosition.name !== PIECES.empty) {
+        return true
+      }
+
+      if (board[toX - 1][toY].name !== PIECES.empty) {
+        return true
+      }
+    }
+
+    if (squaresToMoveX === 1 && squaresToMoveY === 0) {
+      if (
+        moveToBoardPosition.name !== PIECES.empty &&
+        moveToBoardPosition.color !== this.color
+      ) {
+        return true
+      }
+    }
+
+    if (squaresToMoveX === 1 && squaresToMoveY !== 0) {
+      if (moveToBoardPosition.color === this.color) {
+        return true
+      }
+
+      if (moveToBoardPosition.name === PIECES.empty) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  squaresToMove(moveTo: PiecePosition) {
+    const [fromX, fromY] = this.position
+    const [toX, toY] = moveTo
+    const squaresToMoveX = fromX - toX
+    const squaresToMoveY = fromY - toY
+
+    return {
+      squaresToMoveX,
+      squaresToMoveY,
+    }
   }
 }
--- src/game/pieces/Rook.ts
+++ src/game/pieces/Rook.ts
@@ -1,5 +1,6 @@
 import { PIECES } from '../../consts/pieces'
-import { Color, Piece, PieceName, PiecePosition } from '../../types'
+import { ChessBoard, Color, Piece, PieceName, PiecePosition } from '../../types'
+import { Empty } from './Empty'
 
 export class Rook implements Piece {
   public name: PieceName = PIECES.rook
@@ -16,5 +17,79 @@ export class Rook implements Piece {
 
   setPosition(position: PiecePosition) {
     this.position = position
+  }
+
+  movePieceTo(moveTo: PiecePosition, board: ChessBoard): boolean {
+    const [fromX, fromY] = this.position
+    const [toX, toY] = moveTo
+    const { squaresToMoveX, squaresToMoveY } = this.squaresToMove(moveTo)
+    const currentPiece = board[fromX][fromY]
+
+    console.log({ squaresToMoveX, squaresToMoveY })
+    if (squaresToMoveX !== 0 && squaresToMoveY !== 0) {
+      return false
+    }
+
+    if (this.checkColision(moveTo, board)) {
+      console.log('xd')
+      return false
+    }
+
+    board[toX][toY] = currentPiece
+    board[fromX][fromY] = new Empty('empty', [fromX, fromY], '')
+    this.setPosition(moveTo)
+
+    return true
+  }
+
+  checkColision(moveTo: PiecePosition, board: ChessBoard): boolean {
+    const [fromX, fromY] = this.position
+    const { squaresToMoveX, squaresToMoveY } = this.squaresToMove(moveTo)
+
+    if (squaresToMoveX !== 0) {
+      if (squaresToMoveX > 0) {
+        for (let i = 1; i < squaresToMoveX; i++) {
+          if (board[fromX - i][fromY].name !== PIECES.empty) {
+            return true
+          }
+        }
+      } else if (squaresToMoveX < 0) {
+        for (let i = 1; i < -squaresToMoveX; i++) {
+          if (board[fromX + i][fromY].name !== PIECES.empty) {
+            return true
+          }
+        }
+      }
+    }
+
+    if (squaresToMoveY !== 0) {
+      if (squaresToMoveY > 0) {
+        for (let i = 1; i < squaresToMoveY; i++) {
+          if (board[fromX][fromY - i].name !== PIECES.empty) {
+            return true
+          }
+        }
+      } else if (squaresToMoveY < 0) {
+        for (let i = 1; i < -squaresToMoveY; i++) {
+          if (board[fromX][fromY + i].name !== PIECES.empty) {
+            return true
+          }
+        }
+      }
+    }
+
+    return false
+  }
+
+  squaresToMove(moveTo: PiecePosition) {
+    const [fromX, fromY] = this.position
+    const [toX, toY] = moveTo
+    const squaresToMoveX = fromX - toX
+    const squaresToMoveY = fromY - toY
+
+    return {
+      squaresToMoveX,
+      squaresToMoveY,
+    }
   }
 }
