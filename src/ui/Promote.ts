0cb866a919b46b881ae0e475372290214866f5ef
Facug03 <facundogonzales1550@gmail.com>
2024-06-04 17:33:01 -0300

    Promote ui

--- src/ui/Promote.ts
+++ src/ui/Promote.ts
@@ -1,8 +1,41 @@
-export function Promote(): string {
-  return `<div id="promote">
-        <div id="queen" data-piece="queen"></div>
-        <div id="knight" data-piece="knight"></div>
-        <div id="rook" data-piece="rook"></div>
-        <div id="bishop" data-piece="bishop"></div>
-    </div>`
+import { PIECES } from '../consts/pieces'
+import { Color, PieceName } from '../types'
+
+interface Props {
+  color: Color
+}
+
+export function Promote({ color }: Props): string {
+  const isWhite = color === 'white'
+  const boxShadow = isWhite ? 'rgba(0, 0, 0, 0.35) 0px 5px 8px' : 'rgba(0, 0, 0, 0.35) 0px -5px 8px'
+  const borderRadiusTop = isWhite ? '10px 10px 0 0' : '0 0 10px 10px'
+  const borderRadiusBottom = isWhite ? '0 0 10px 10px' : '10px 10px 0 0'
+  const bgImage = (piece: PieceName) => `/assets/pieces/${color}/${piece}.png`
+
+  return `
+  <div
+    class="promote queen" 
+    data-piece="${PIECES.queen}"
+    style="background-image: url(${bgImage('queen')}); background-color: #f9f9f9; top: ${isWhite ? '0' : '0'}; box-shadow: ${boxShadow}; border-radius: ${borderRadiusTop};"
+  >
+  </div>
+  <div 
+    class="promote knight" 
+    data-piece="${PIECES.knight}" 
+    style="background-image: url(${bgImage('knight')}); background-color: #f9f9f9; top: ${isWhite ? '100%' : '-100%'}; box-shadow: ${boxShadow};"
+  >
+  </div>
+  <div 
+    class="promote rook" 
+    data-piece="${PIECES.rook}" 
+    style="background-image: url(${bgImage('rook')}); background-color: #f9f9f9; top: ${isWhite ? '200%' : '-200%'}; box-shadow: ${boxShadow};"
+  >
+  </div>
+  <div 
+    class="promote bishop" 
+    data-piece="${PIECES.bishop}" 
+    style="background-image: url(${bgImage('bishop')}); background-color: #f9f9f9; top: ${isWhite ? '300%' : '-300%'}; box-shadow: ${boxShadow}; border-radius: ${borderRadiusBottom};"
+  >
+  </div>
+  `
 }
