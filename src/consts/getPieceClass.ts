efeafcffd3b26b531ac45f8d7d98114d36be53e8
Facug03 <facundogonzales1550@gmail.com>
2024-06-05 23:07:11 -0300

    change name of const

--- src/consts/getPieceClass.ts
+++ src/consts/getPieceClass.ts
@@ -2,10 +2,16 @@ import { Queen } from '../game/pieces/Queen'
 import { Knight } from '../game/pieces/Knight'
 import { Rook } from '../game/pieces/Rook'
 import { Bishop } from '../game/pieces/Bishop'
+import { Empty } from '../game/pieces/Empty'
+import { Pawn } from '../game/pieces/Pawn'
+import { King } from '../game/pieces/King'
 
-export const getPromoteClass = {
+export const getPieceClass = {
   queen: Queen,
   knight: Knight,
   rook: Rook,
   bishop: Bishop,
+  empty: Empty,
+  pawn: Pawn,
+  king: King,
 }
