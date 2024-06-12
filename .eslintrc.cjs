fce52a4ff233ad687e56a176c9d4559fd4bcaf05
Facug03 <facundogonzales1550@gmail.com>
2024-06-12 18:06:51 -0300

    fix no unusued vars for ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,10 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'standard', 'prettier'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: [],
   rules: {
-    'no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
     'semi': ['warn', 'never'],
     'no-eval': 'off',
     'no-undef': 'off',
