32e5eb089b2cc07f234ef263e7fce93fb76b977e
Facug03 <facundogonzales1550@gmail.com>
2024-06-12 18:09:49 -0300

    add loggers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
 import { env } from '@/common/utils/envConfig'
-import { app } from '@/server'
+import { app, logger } from '@/server'
 
 const server = app.listen(env.PORT, () => {
   const { NODE_ENV, HOST, PORT } = env
-  console.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`)
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`)
 })
 
 const onCloseSignal = () => {
-  console.info('sigint received, shutting down')
+  logger.info('sigint received, shutting down')
   server.close(() => {
-    console.info('server closed')
+    logger.info('server closed')
     process.exit()
   })
   setTimeout(() => process.exit(1), 10000).unref() // Force shutdown after 10s
