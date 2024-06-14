e9521efdd3a0c3c00cfe69c8cc50792d96fbc545
Facug03 <facundogonzales1550@gmail.com>
2024-06-14 19:09:20 -0300

    positions dictionary

---
+++ frontend/src/consts/positionsMap.ts
@@ -0,0 +1,43 @@
+export const POSITIONS_MAP_Y: Record<string, string> = {
+  '0': 'a',
+  '1': 'b',
+  '2': 'c',
+  '3': 'd',
+  '4': 'e',
+  '5': 'f',
+  '6': 'g',
+  '7': 'h',
+}
+
+export const POSITIONS_MAP_X: Record<string, string> = {
+  '0': '8',
+  '1': '7',
+  '2': '6',
+  '3': '5',
+  '4': '4',
+  '5': '3',
+  '6': '2',
+  '7': '1',
+}
+
+export const POSITIONS_MAP_X_REVERSE: Record<string, number> = {
+  '8': 0,
+  '7': 1,
+  '6': 2,
+  '5': 3,
+  '4': 4,
+  '3': 5,
+  '2': 6,
+  '1': 7,
+}
+
+export const POSITIONS_MAP_Y_REVERSE: Record<string, number> = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  e: 4,
+  f: 5,
+  g: 6,
+  h: 7,
+}
