5ff3be0f644628819ddb7ecbf421e44352b4b8d1
Facug03 <facundogonzales1550@gmail.com>
2024-06-20 17:35:55 -0300

    set host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1'
+  },
   resolve: {
     alias: {
       '@src': fileURLToPath(new URL('./src', import.meta.url)),
-      '@lib': fileURLToPath(new URL('./lib', import.meta.url)),
-    },
-  },
+      '@lib': fileURLToPath(new URL('./lib', import.meta.url))
+    }
+  }
 })
