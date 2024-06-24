5e6e8760f7220277b05c558d17855957509f419c
Facug03 <facundogonzales1550@gmail.com>
2024-06-23 22:32:20 -0300

    test only on chromium

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -36,17 +36,17 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
-    },
+    }
 
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] }
     // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    }
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] }
+    // }
 
     /* Test against mobile viewports. */
     // {
